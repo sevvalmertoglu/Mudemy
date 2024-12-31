0e73dd0c86b2e82b25be4fdc3776567149c2f7f1
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2024-12-31 03:59:04 +0300

    Added DtoMapper for mapping between DTOs and services

---
+++ Mudemy.Service/DtoMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Mudemy.Core.DTOs;
+using Mudemy.Core.Models;
+
+namespace Mudemy.Service
+{
+    internal class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<CourseDto, Course>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
