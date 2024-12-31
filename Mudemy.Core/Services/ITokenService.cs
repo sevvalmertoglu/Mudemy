820db2bd0fef3ff7317755f464085ea3728b3360
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2024-12-31 03:56:31 +0300

    Added repository and service interfaces

---
+++ Mudemy.Core/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mudemy.Core.DTOs;
+using Mudemy.Core.Models;
+
+namespace Mudemy.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+    }
+}
