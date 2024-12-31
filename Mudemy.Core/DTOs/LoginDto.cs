7bbda42557230a87f2fcecc2f2a4de9454de4500
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2024-12-31 03:56:05 +0300

    Added and updated DTOs

---
+++ Mudemy.Core/DTOs/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mudemy.Core.DTOs
+{
+    public class LoginDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ Mudemy.Core/DTOs/TokenDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mudemy.Core.DTOs
+{
+    public class TokenDto
+    {
+        public string AccessToken { get; set; } = default!;
+
+        public DateTime AccessTokenExpiration { get; set; }
+
+        public string RefreshToken { get; set; } = default!;
+
+        public DateTime RefreshTokenExpiration { get; set; }
+    }
+}
