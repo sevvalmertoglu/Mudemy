127908bd8cf132acfa58e75f530b9d2a0c341dd5
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2025-01-02 21:10:52 +0300

    added required

--- SharedLibrary/Configurations/CustomTokenOption.cs
+++ SharedLibrary/Configurations/CustomTokenOption.cs
@@ -6,12 +6,12 @@ namespace SharedLibrary.Configurations
 {
     public class CustomTokenOption
     {
-        public List<String> Audience { get; set; }
-        public string Issuer { get; set; }
+        public required List<String> Audience { get; set; }
+        public required string Issuer { get; set; }
 
         public int AccessTokenExpiration { get; set; }
         public int RefreshTokenExpiration { get; set; }
 
-        public string SecurityKey { get; set; }
+        public required string SecurityKey { get; set; }
     }
 }
