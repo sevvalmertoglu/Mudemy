6271a467c910d63494f271a317077e96a722099c
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2025-01-03 00:50:50 +0300

    I added protected

--- Mudemy.Data/Repositories/GenericRepository.cs
+++ Mudemy.Data/Repositories/GenericRepository.cs
@@ -11,7 +11,7 @@ namespace Mudemy.Data.Repositories
 {
     public class GenericRepository<Tentity> : IGenericRepository<Tentity> where Tentity : class
     {
-        protected readonly DbContext _context;
+        private readonly DbContext _context;
         private readonly DbSet<Tentity> _dbSet;
 
         public GenericRepository(AppDbContext context)
