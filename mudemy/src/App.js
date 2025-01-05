c25e73e02b709a5f087a36044f1b6d6a05ebdc6b
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2025-01-05 15:26:02 +0300

    Added style for footer

--- mudemy/src/App.js
+++ mudemy/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {AuthProvider} from "./context/AuthContext";
-import {CartProvider} from "./context/CartContext";
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -8,26 +8,30 @@ import PaymentForm from "./pages/PaymentForm";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Cart from "./pages/Cart";
-import ProductDetail from "./pages/ProductDetail";
-import Register from './pages/Register';
+import CourseDetail from "./pages/CourseDetail";
+import Register from "./pages/Register";
+import "./App.css";
 
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <AuthProvider>
       <BrowserRouter>
         <CartProvider>
-          <Navbar/>
-          <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/cart" element={<Cart/>}></Route>
-            <Route path="/profile" element={<Profile/>}></Route>
-            <Route path="/login" element={<Login/>}></Route>
-            <Route path="/payment" element={<PaymentForm/>}></Route>
-            <Route path="/product/:id" element={<ProductDetail/>}></Route>
-            <Route path="/register" element={<Register/>}></Route>
-          </Routes>
-          <Footer></Footer>
+          <div className="App">
+            <Navbar />
+            <div className="App-main">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/payment" element={<PaymentForm />} />
+                <Route path="/product/:id" element={<CourseDetail />} />
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </div>
+            <Footer className="App-footer" />
+          </div>
         </CartProvider>
       </BrowserRouter>
     </AuthProvider>
